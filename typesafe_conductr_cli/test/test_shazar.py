abe74aded4c1dad7e32b53e56bc82e1a774af56f
Heiko Seeberger <mail@heikoseeberger.de>
2015-01-21 13:19:37 +0100

    Replace shatar with shazar

--- typesafe_conductr_cli/test/test_shazar.py
+++ typesafe_conductr_cli/test/test_shazar.py
@@ -1,22 +1,20 @@
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
 import tempfile
 from os import remove
-from typesafe_conductr_cli.shatar import digest_file, build_parser
+from typesafe_conductr_cli.shazar import create_digest, build_parser
 
-class TestShatar(TestCase):
 
-    def test_digest_file(self):
+class TestShazar(TestCase):
+
+    def test_create_digest(self):
         temp = tempfile.NamedTemporaryFile(mode='w+b', delete=False)
         temp.write(b"test file data")
         temp_name = temp.name
         temp.close()
-
         self.assertEqual(
-            digest_file(temp_name),
+            create_digest(temp_name),
             "1be7aaf1938cc19af7d2fdeb48a11c381dff8a98d4c4b47b3b0a5044a5255c04"
         )
-
         remove(temp_name)
 
     def test_parser_success(self):
