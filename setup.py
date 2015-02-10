d432f1c51f1c42e43366c41ca8e40d89f9fdc432
Martynas Mickevičius <mmartynas@gmail.com>
2015-02-10 09:18:05 +0200

    Version 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='typesafe-conductr-cli',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
 
     entry_points={
