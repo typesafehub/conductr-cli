7e223a818c6a85fd0e01a4d6d7516a71bd29ff53
Markus Jura <markus.jura@gmail.com>
2016-11-17 19:55:20 +0100

    Display ansi colors on Windows correctly

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ install_requires = [
     'requests-toolbelt>=0.7.0',
     'argcomplete>=0.8.1',
     'pyhocon==0.2.1',
-    'arrow>=0.6.0',
+    'arrow>=0.6.0'
+    'colorama>=0.3.7',
 
     # FIXME: Remove the following dependencies when dcos can be depended on
     'jsonschema==2.4',  # pin the exact version, jsonschema 2.5 broke py3
