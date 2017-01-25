1552ab6732855280c8edd0f8f67102b5a35ec489
Felix Satyaputra <felix.satyaputra@typesafe.com>
2017-01-25 12:02:45 +1100

    Add service locator url column to conduct service-names command

--- conductr_cli/conduct_url.py
+++ conductr_cli/conduct_url.py
@@ -9,6 +9,14 @@ def conductr_host(args):
     return vars(args).get('host') or vars(args).get('ip')
 
 
+def service_locator_url(service_name, args):
+    if service_name:
+        base_url = '{}://{}:{}{}'.format(args.scheme, conductr_host(args), 9008, args.base_path)
+        return '{}services/{}'.format(base_url, service_name)
+    else:
+        return ''
+
+
 def api_version_path(api_version):
     if api_version == '1':
         return ''
