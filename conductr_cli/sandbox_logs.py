046cfeaf103cc5333bb511ba1996ef3ba853b711
Jason Longshore <longshorej@gmail.com>
2017-01-25 11:16:41 -0600

    implement `sandbox logs` for collecting logs from agent and core processes

---
+++ conductr_cli/sandbox_logs.py
@@ -0,0 +1,24 @@
+import os
+
+def logs_args(args):
+    core_log = os.path.abspath('{}/core/logs/conductr.log'.format(args.image_dir))
+    agent_log = os.path.abspath('{}/agent/logs/conductr-agent.log'.format(args.image_dir))
+
+    tail_args = ['/usr/bin/env', 'tail', '-q']
+
+    if args.follow:
+        tail_args.append('-f')
+        tail_args.append('--follow=name')
+        tail_args.append('--retry')
+
+    if args.lines:
+        tail_args.append('-n')
+        tail_args.append(str(args.lines))
+
+    tail_args.append(core_log)
+    tail_args.append(agent_log)
+
+    return tail_args
+
+def logs(args):
+    os.execv('/usr/bin/env', logs_args(args))
---
+++ conductr_cli/test/test_sandbox_logs.py
@@ -0,0 +1,26 @@
+from conductr_cli.test.cli_test_case import CliTestCase
+from conductr_cli import sandbox_logs
+from unittest.mock import MagicMock
+
+class TestSandboxLogs(CliTestCase):
+    def test_tail_args_built(self):
+        self.assertEqual(
+            ['/usr/bin/env', 'tail', '-q', '-n', '15', '/image/dir/core/logs/conductr.log', '/image/dir/agent/logs/conductr-agent.log'],
+
+            sandbox_logs.logs_args(MagicMock(**{
+                'image_dir': '/image/dir',
+                'follow': False,
+                'lines': 15
+            }))
+        )
+
+
+        self.assertEqual(
+            ['/usr/bin/env', 'tail', '-q', '-f', '--follow=name', '--retry', '-n', '35', '/some/image/dir/core/logs/conductr.log', '/some/image/dir/agent/logs/conductr-agent.log'],
+
+            sandbox_logs.logs_args(MagicMock(**{
+                'image_dir': '/some/image/dir',
+                'follow': True,
+                'lines': 35
+            }))
+        )
