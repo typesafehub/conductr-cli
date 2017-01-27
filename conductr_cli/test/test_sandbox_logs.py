0197c957a95aadc5bd70fa05e362130e768ec335
Jason Longshore <longshorej@gmail.com>
2017-01-27 15:24:49 -0600

    implement `sandbox logs` for collecting logs from agent and core processes

--- conductr_cli/test/test_sandbox_logs.py
+++ conductr_cli/test/test_sandbox_logs.py
@@ -2,25 +2,35 @@ from conductr_cli.test.cli_test_case import CliTestCase
 from conductr_cli import sandbox_logs
 from unittest.mock import MagicMock
 
+import io
+import tempfile
+
 
 class TestSandboxLogs(CliTestCase):
-    def test_tail_args_built(self):
+    def test_log_files_is_correct(self):
         self.assertEqual(
-            ['/usr/bin/env', 'tail', '-q', '-n', '15', '/image/dir/core/logs/conductr.log', '/image/dir/agent/logs/conductr-agent.log'],
+            ['/image/dir/core/logs/conductr.log', '/image/dir/agent/logs/conductr-agent.log'],
 
-            sandbox_logs.logs_args(MagicMock(**{
-                'image_dir': '/image/dir',
-                'follow': False,
-                'lines': 15
-            }))
+            sandbox_logs.log_files(MagicMock(**{'image_dir': '/image/dir'}))
         )
 
-        self.assertEqual(
-            ['/usr/bin/env', 'tail', '-q', '-f', '--follow=name', '--retry', '-n', '35', '/some/image/dir/core/logs/conductr.log', '/some/image/dir/agent/logs/conductr-agent.log'],
+    def test_tail_reads_files(self):
+        one_fd, one_path = tempfile.mkstemp()
+        two_fd, two_path = tempfile.mkstemp()
 
-            sandbox_logs.logs_args(MagicMock(**{
-                'image_dir': '/some/image/dir',
-                'follow': True,
-                'lines': 35
-            }))
+        one_file = open(one_path, 'w')
+        two_file = open(two_path, 'w')
+
+        output = io.StringIO()
+
+        one_file.write("line 1\nline 2\nline 3\n")
+        one_file.close()
+        two_file.write("line a\nline b\nline c\n")
+        two_file.close()
+
+        sandbox_logs.tail([one_path, two_path], False, output, 8, 0.25)
+
+        self.assertEqual(
+            "line 1\nline 2\nline 3\nline a\nline b\nline c\n",
+            output.getvalue()
         )
