74cdfe830e01f80b2e00b761b0e04a600ca93cde
Jason Longshore <longshorej@gmail.com>
2017-04-11 12:46:15 -0500

    conduct setup-dcos improved to detect conduct location when not on path and deal with broken symlinks

--- conductr_cli/conduct_dcos.py
+++ conductr_cli/conduct_dcos.py
@@ -2,12 +2,22 @@ from dcos import constants
 import logging
 import os
 import shutil
+import sys
 
 
 def setup(args):
     """`conduct setup-dcos` command"""
     log = logging.getLogger(__name__)
-    src = shutil.which("conduct")
+
+    if sys.executable.endswith(os.path.sep + 'conduct'):
+        src = sys.executable
+    else:
+        src = shutil.which('conduct')
+
+        if src is None:
+            log.error('Unable to determine location of \'conduct\'; is it on the PATH?')
+            return False
+
     dst = os.path.join(os.path.expanduser('~'),
                        constants.DCOS_DIR,
                        constants.DCOS_SUBCOMMAND_SUBDIR,
@@ -15,10 +25,11 @@ def setup(args):
                        constants.DCOS_SUBCOMMAND_ENV_SUBDIR,
                        'bin',
                        constants.DCOS_COMMAND_PREFIX + 'conduct')
-    if os.path.exists(dst):
+
+    if os.path.exists(dst) or os.path.islink(dst):
         os.remove(dst)
-    else:
-        os.makedirs(os.path.dirname(dst), exist_ok=True)
+
+    os.makedirs(os.path.dirname(dst), exist_ok=True)
     os.symlink(src, dst)
     log.screen('The DC/OS CLI is now configured.\n'
                'Prefix \'conduct\' with \'dcos\' when you want to contact ConductR on DC/OS e.g. \'dcos conduct info\'')
