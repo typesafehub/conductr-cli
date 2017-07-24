cd444804588f84f2d7c3c013556a46d4bb4c7d23
ashutosh raina <ashutoshraina1989@gmail.com>
2017-07-23 19:05:25 -0700

    extract out bundle stop into control protocol.

--- conductr_cli/control_protocol.py
+++ conductr_cli/control_protocol.py
@@ -2,6 +2,7 @@ import logging
 
 from conductr_cli import conduct_url, conduct_request, validation
 from conductr_cli.conduct_url import conductr_host
+from conductr_cli.http import DEFAULT_HTTP_TIMEOUT
 
 
 def load_bundle(args, multipart_files):
@@ -13,6 +14,20 @@ def load_bundle(args, multipart_files):
                                     auth=args.conductr_auth,
                                     verify=args.server_verification_file,
                                     headers={'Content-Type': multipart_files.content_type})
+    validation.raise_for_status_inc_3xx(response)
+
+    if log.is_verbose_enabled():
+        log.verbose(validation.pretty_json(response.text))
+
+    return response.text
+
+
+def stop_bundle(args):
+    log = logging.getLogger(__name__)
+    path = 'bundles/{}?scale=0'.format(args.bundle)
+    url = conduct_url.url(path, args)
+    response = conduct_request.put(args.dcos_mode, conductr_host(args), url, auth=args.conductr_auth,
+                                   verify=args.server_verification_file, timeout=DEFAULT_HTTP_TIMEOUT)
     validation.raise_for_status_inc_3xx(response)
 
     if log.is_verbose_enabled():
--- conductr_cli/test_control_protocol.py
+++ conductr_cli/test_control_protocol.py
@@ -3,13 +3,14 @@ from unittest.mock import patch, MagicMock
 from requests import HTTPError
 
 from conductr_cli import logging_setup
-from conductr_cli.control_protocol import load_bundle
+from conductr_cli.control_protocol import load_bundle, stop_bundle
 from conductr_cli.test.cli_test_case import CliTestCase
 
 
 class TestControlProtocol(CliTestCase):
     conductr_auth = ('username', 'password')
     server_verification_file = MagicMock(name='server_verification_file')
+    bundle_id = '45e0c477d3e5ea92aa8d85c0d8f3e25c'
     args = {
         'dcos_mode': False,
         'command': 'conduct',
@@ -23,7 +24,7 @@ class TestControlProtocol(CliTestCase):
         'no_wait': False,
         'quiet': False,
         'cli_parameters': '',
-        'bundle': '45e0c477d3e5ea92aa8d85c0d8f3e25c',
+        'bundle': bundle_id,
         'output_path': '/my/fav/path',
         'conductr_auth': conductr_auth,
         'server_verification_file': server_verification_file
@@ -65,3 +66,38 @@ class TestControlProtocol(CliTestCase):
                                               auth=self.conductr_auth, data=file_mock,
                                               verify=self.server_verification_file,
                                               headers={'Content-Type': file_mock.content_type})
+
+    def test_stop_bundle_success(self):
+        stdout = MagicMock()
+        stderr = MagicMock()
+        logging_setup.configure_logging(MagicMock(), stdout, stderr)
+
+        args_mock = MagicMock(**self.args)
+        stop_bundle_url = 'http://127.0.0.1:9005/bundles/{}?scale=0'.format(self.bundle_id)
+        post_mock = self.respond_with(status_code=200, text='{}')
+
+        with patch('conductr_cli.conduct_request.put', post_mock):
+            result = stop_bundle(args_mock)
+
+        post_mock.assert_called_once_with(False, '127.0.0.1', stop_bundle_url,
+                                          auth=self.conductr_auth, timeout=5,
+                                          verify=self.server_verification_file
+                                          )
+        self.assertEqual('{}', result)
+
+    def test_stop_bundle_failure(self):
+        stdout = MagicMock()
+        stderr = MagicMock()
+        logging_setup.configure_logging(MagicMock(), stdout, stderr)
+
+        args_mock = MagicMock(**self.args)
+        stop_bundle_url = 'http://127.0.0.1:9005/bundles/{}?scale=0'.format(self.bundle_id)
+        post_mock = self.respond_with(status_code=401)
+
+        with patch('conductr_cli.conduct_request.put', post_mock):
+            self.assertRaises(HTTPError, lambda: stop_bundle(args_mock))
+
+        post_mock.assert_called_once_with(False, '127.0.0.1', stop_bundle_url,
+                                          auth=self.conductr_auth, timeout=5,
+                                          verify=self.server_verification_file
+                                          )
